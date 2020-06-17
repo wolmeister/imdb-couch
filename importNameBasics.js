03aa4ea59e057b8a8a7431fc4d7792e22e63b936
Victor Wolmeister <wolmeistervictor@gmail.com>
2020-06-17 18:52:08 -0300

    change to mongo

--- importNameBasics.js
+++ importNameBasics.js
@@ -1,8 +1,10 @@
-const nano = require('nano')('http://admin:admin@localhost:5984');
+// const nano = require('nano')('http://admin:admin@localhost:5984');
+const MongoClient = require('mongodb').MongoClient;
 const mysql = require('mysql2/promise');
 const _ = require('lodash');
 
 const limit = 100;
+const client = new MongoClient('mongodb://localhost:27017');
 
 async function doWork() {
   const sqlDb = await mysql.createPool({
@@ -12,16 +14,22 @@ async function doWork() {
     database: 'imdb2',
     connectionLimit: 50,
   });
-  
+
+  await client.connect();
+  const mongoDb = client.db('imdb');
+  const collection = mongoDb.collection('name_basics');
+
   //clear database
-  await nano.db.destroy('imdb2');
-  await nano.db.create('imdb2');
-  const couchDb = nano.use('imdb2');
+  // await nano.db.destroy('imdb2');
+  // await nano.db.create('imdb2');
+  // const couchDb = nano.use('imdb2');
 
   let time = Date.now();
 
   console.log('querying count...');
-  const [maxRows] = await sqlDb.query(`select max(nconst) as max_nconst from name_basics`);
+  const [maxRows] = await sqlDb.query(
+    `select max(nconst) as max_nconst from name_basics`
+  );
   const maxNconst = maxRows[0]['max_nconst'];
   console.log('finished count', maxNconst, '-', Date.now() - time);
   const offsets = [];
@@ -30,19 +38,31 @@ async function doWork() {
     offsets.push(offset);
   }
 
-  console.log(`offsets ${offsets.length}`)
+  console.log(`offsets ${offsets.length}`);
 
   for (const offset of offsets) {
     time = Date.now();
     console.log('querying name_basics offset', offset);
-    const [rowsBasics] = await sqlDb.query(`select * from name_basics where nconst > ${offset} and nconst < ${offset + limit}`);
+    const [rowsBasics] = await sqlDb.query(
+      `select * from name_basics where nconst > ${offset} and nconst < ${
+        offset + limit
+      }`
+    );
     console.log('finished querying name_basics offset', '-', Date.now() - time);
 
     time = Date.now();
     console.log('querying title_principals offset', offset);
-    const [rowsPrincipals] = await sqlDb.query(`select * from title_principals where nconst > ${offset} and nconst < ${offset + limit}`);
-    console.log('finished querying title_principals offset', '-', Date.now() - time);
-    
+    const [rowsPrincipals] = await sqlDb.query(
+      `select * from title_principals where nconst > ${offset} and nconst < ${
+        offset + limit
+      }`
+    );
+    console.log(
+      'finished querying title_principals offset',
+      '-',
+      Date.now() - time
+    );
+
     const principalsMap = [];
 
     time = Date.now();
@@ -78,12 +98,14 @@ async function doWork() {
     console.log('inserting into couchdb...');
 
     for (const chunk of _.chunk(rowsBasics, 100)) {
-      await couchDb.bulk({
-        docs: chunk,
-      });
+      collection.insertMany(chunk);
+      // await couchDb.bulk({
+      //   docs: chunk,
+      // });
     }
 
-    console.log('finished inserting into couchdb', '-', Date.now() - time);
+    console.log('finished inserting into mongodb', '-', Date.now() - time);
+    // console.log('finished inserting into couchdb', '-', Date.now() - time);
   }
 
   // console.log('finished sql...');
